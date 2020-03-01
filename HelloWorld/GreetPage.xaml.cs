05523abbebeaf2fbb6c26ffb419cfb4c14b77c47
Anthony Howe <anthowe33@gmail.com>
2020-03-01 12:21:58 -0800

    First Commit

---
+++ HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+        [Obsolete]
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+            if (Device.OS == TargetPlatform.iOS)
+                Padding = new Thickness(0, 40, 0, 0);
+            else if (Device.OS == TargetPlatform.Android)
+                Padding = new Thickness(200, 40, 0, 0);
+            else if (Device.OS == TargetPlatform.Windows)
+                Padding = new Thickness(0, 40, 0, 0);
+
+
+
+            Device.OnPlatform(
+                iOS: new Thickness(0, 40, 0, 0),
+                Android: new Thickness(200, 40, 0, 0),
+                WinPhone: new Thickness(0, 0, 0, 0);
+
+
+        }
+
+
+    }
+
+
+
+}
+
