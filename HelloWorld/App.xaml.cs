5a3b70a20676166b3616e1e2847f19bc654221b9
Anthony Howe <anthowe33@gmail.com>
2020-03-05 17:21:36 -0800

    Next Commit

--- HelloWorld/App.xaml.cs
+++ HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new GreetPage();
+            MainPage = new AbsolutePage();
         }
 
         protected override void OnStart()
